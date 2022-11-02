5a5c2e7d264468e2ea3cd327e703a104761da113
= <=>
2022-11-02 23:01:11 +0700

    [33] Get Book

--- store/views.py
+++ store/views.py
@@ -41,7 +41,7 @@ def store(request, category_slug=None):
 def product_detail(request, category_slug, product_slug):
     try:
         single_product = Product.objects.get(
-            category__slug=category_slug, slug=product_slug)
+            category__slug=category_slug, slug=product_slug) #Get Book
         in_cart = CartItem.objects.filter(cart__cart_id=_cart_id(
             request), product=single_product).exists()
 
