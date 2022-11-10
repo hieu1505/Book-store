65ef0a5dbd7191df778341def0035b1a198d42e7
= <=>
2022-11-10 21:51:18 +0700

    [52] Total Payment

--- carts/views.py
+++ carts/views.py
@@ -186,7 +186,7 @@ def cart(request, total=0, quantity=0, cart_items=None):
         'quantity': quantity,
         'cart_items': cart_items,
         'tax': tax,
-        'grand_total': grand_total,
+        'grand_total': grand_total, #product price and tax
     }
 
     return render(request, 'store/cart.html', context)
