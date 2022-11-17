5972909f75f9ab084e38466991f9e9a1e165456e
= <=>
2022-11-17 19:45:35 +0700

    [59] Create Payment With Current Information

--- orders/views.py
+++ orders/views.py
@@ -19,7 +19,7 @@ def payments(request):
         payment_method = body['payment_method'],
         amount_paid = order.order_total,
     )
-    payment.save()
+    payment.save() #create payment
     order.payment = payment
     order.is_ordered = True
     order.save()
