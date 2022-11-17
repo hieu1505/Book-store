3acb34473f1806b32518a23ae85bd70ffec0d3f1
Cao Minh Tri <caominhtri112201@gmail.com>
2022-11-17 10:52:57 +0700

    [57] Handle empty cart

--- orders/views.py
+++ orders/views.py
@@ -71,7 +71,7 @@ def payments(request):
 
     return JsonResponse(data)
 
-
+#  Handle empty cart -> return to shop
 def place_order(request, total=0, quantity=0,):
     current_user = request.user
 
